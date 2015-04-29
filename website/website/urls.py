aa7d62cf845ca974bae593e6d53a834c1749fabc
Isaac Jordan <sheepses6@gmail.com>
2015-04-29 21:14:55 +0100

    Added static files, and bubblesort algorithm.

--- website/website/urls.py
+++ website/website/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'website.views.index', name='index'),
+    url(r'^bubblesort/$', 'website.views.bubblesort', name='bubblesort'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- website/website/views.py
+++ website/website/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def bubblesort(request):
+    return render(request, "bubblesort.html")
